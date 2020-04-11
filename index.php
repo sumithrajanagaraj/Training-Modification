710e812f18520a5d9b6ec65d55a1ba80c375b6ed
Venkataramanan S <venkataramanan.cs@gmail.com>
2020-04-11 17:02:38 +0530

    First Commit for training purpose

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRACTICE</title>
+</head>
+<body>
+<?php 
+$signal = 1;
+if($signal == 0){
+	?>
+<div class="container"style="height: 150px; background-color: red;"></div>
+<?php 
+} 
+elseif ($signal==1){
+	?>
+<div class="container"style="height: 150px; background-color: green;"></div>
+<?php }
+else{ ?>
+<div class="container"style="height: 150px; background-color: orange;"></div><?php }?>
+</body>
+</html>
